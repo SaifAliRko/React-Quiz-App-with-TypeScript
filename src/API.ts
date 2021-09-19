035331d13a8cfc99553bec409e36cbda5c8ebdf7
SaifAliRko <saif.ali96587@gmail.com>
2021-09-19 07:06:05 +0200

    Updating and deploying the App

---
+++ src/API.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+export const shuffleArray = (array: string[]) => {
+    return [...array].sort(() => Math.random() - 0.5);
+};
+
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export type QuestionsState = Question & { answers: string[] };
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+  }))
+};
